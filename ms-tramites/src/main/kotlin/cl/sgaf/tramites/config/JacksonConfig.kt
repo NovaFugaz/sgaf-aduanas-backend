96a3650e90bcaa34597d15b6ee6ac2ee13236d6f
Ignacia Astrea <svt.nova@proton.me>
2026-07-07 11:24:32 -0400

    fix: docker build ms issues

---
+++ ms-tramites/src/main/kotlin/cl/sgaf/tramites/config/JacksonConfig.kt
@@ -0,0 +1,18 @@
+package cl.sgaf.tramites.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class JacksonConfig {
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        val om = ObjectMapper()
+        om.registerModule(JavaTimeModule())
+        om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        return om
+    }
+}
